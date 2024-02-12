f2a289eb06c3fd206129163e611bb4cc2ca5d3ee
tjasa <tjasa.zilavec@gmail.com>
2024-02-12 16:41:00 +0100

    added angularfire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+/**DevExtreme*/
 import { DxFormModule } from 'devextreme-angular';
 import { DxChartModule } from 'devextreme-angular';
 import { DxButtonModule } from 'devextreme-angular';
+
+/**Firebase*/
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from './environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +21,8 @@ import { DxButtonModule } from 'devextreme-angular';
     DxFormModule,
     DxChartModule,
     DxButtonModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDnqolaJx_MqvtZmk20VVft7H2681N45BA',
+    authDomain: 'financetracker-970ea.firebaseapp.com',
+    projectId: 'financetracker-970ea',
+    storageBucket: 'financetracker-970ea.appspot.com',
+    messagingSenderId: '199184969284',
+    appId: '1:199184969284:web:3160fde27c34c103705611',
+  },
+};
