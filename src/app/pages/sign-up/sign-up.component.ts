215be1cf4053a706ec5d1bf55f8d476ea9d1c138
Tjasa Zilavec <tjasa.zilavec@creative37.com>
2024-02-13 10:42:05 +0100

    register with email and signin with email work

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DxFormModule } from 'devextreme-angular';
+import { DxChartModule } from 'devextreme-angular';
+import { DxButtonModule } from 'devextreme-angular';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [DxFormModule, DxChartModule, DxButtonModule],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.scss'
+  styleUrl: '../../app.component.scss'
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+  constructor(private router: Router, private authservice: AuthService) {}
+  ngOnInit(): void {}
+  registerCredentials = {
+    email: '',
+    password: '',
+  };
+  registerButtonOptions = {
+    text: 'Sign Up',
+    useSubmitBehavior: true,
+  };
 
+  registerWithEmailAndPassword() {
+    const { email, password } = this.registerCredentials;
+    this.authservice
+      .registerWithEmailAndPassword({ email, password })
+      .then((response: any) => {
+        this.router.navigateByUrl('/dashboard');
+      })
+      .catch((error: any) => {
+        alert(error);
+      });
+  }
+
+  goToLogin() {
+    this.router.navigateByUrl('/');
+  }
+
+  logWithGoogle() {
+    this.authservice
+      .signInWithGoogle()
+      .then((response: any) => {
+        this.router.navigateByUrl('/dashboard');
+      })
+      .catch((error: any) => {
+        alert(error);
+      });
+  }
 }
