cef1e87ed364cf5003d1a212823abe0115a8485a
Tjasa Zilavec <tjasa.zilavec@creative37.com>
2024-02-13 13:37:10 +0100

    nav closes on btn

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -2,23 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { DxButtonModule } from 'devextreme-angular';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [DxButtonModule],
+  imports: [DxButtonModule, CommonModule],
   templateUrl: './navigation.component.html',
   styleUrl: '../../app.component.scss',
 })
 export class NavigationComponent implements OnInit {
+  navClosed:boolean=true;
   constructor(private router: Router, private afAuth:AngularFireAuth) {}
   ngOnInit(): void {}
   goToLogin() {
     this.afAuth.signOut().then(() => {
-      this.router.navigate(['/']); // Redirect to login or home page
+      this.router.navigate(['/']);
     }).catch(error => {
-      // Handle any errors here
       console.error('Logout error', error);
     });
   }
-
+closeNav(){
+  this.navClosed=!this.navClosed;
+  console.log(this.navClosed)
+}
 }
